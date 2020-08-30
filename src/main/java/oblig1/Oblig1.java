c8869fe3cc9ea74c90246da900e1ccfd397084d5
Vilde Fauchald <vilde.fauchald@gmail.com>
2020-08-30 21:38:58 +0200

    firt commit

---
+++ src/main/java/oblig1/Oblig1.java
@@ -0,0 +1,102 @@
+package oblig1;
+
+import java.lang.UnsupportedOperationException;
+
+////// Løsningsforslag Oblig 1 ////////////////////////
+
+public class Oblig1 {
+
+
+    private Oblig1() {
+    }
+
+    ///// Oppgave 1 //////////////////////////////////////
+    public static int maks(int[] a) {
+        if (a.length == 0) {
+            throw new NullPointerException("Arrayet er tomt");
+        }
+        int maks = a[0];
+
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] > maks) {
+                maks = a[i];
+
+            }
+        }
+        return maks;
+    }
+
+    // returnere hvor mange ganger man mp flytte på tall. Men ikke sortere den!
+    public static int ombyttinger(int[] a) {
+        int temp;
+        int antall = 0;
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                temp = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = temp;
+                antall++;
+            }
+        }
+        return antall;
+    }
+
+
+    ///// Oppgave 2 //////////////////////////////////////
+    public static int antallUlikeSortert(int[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 3 //////////////////////////////////////
+    public static int antallUlikeUsortert(int[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 4 //////////////////////////////////////
+    public static void delsortering(int[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 5 //////////////////////////////////////
+    public static void rotasjon(char[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 6 //////////////////////////////////////
+    public static void rotasjon(char[] a, int k) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 7 //////////////////////////////////////
+    /// 7a)
+    public static String flett(String s, String t) {
+        throw new UnsupportedOperationException();
+    }
+
+    /// 7b)
+    public static String flett(String... s) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 8 //////////////////////////////////////
+    public static int[] indekssortering(int[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 9 //////////////////////////////////////
+    public static int[] tredjeMin(int[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    ///// Oppgave 10 //////////////////////////////////////
+    public static int bokstavNr(char bokstav) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean inneholdt(String a, String b) {
+        throw new UnsupportedOperationException();
+    }
+
+}  // Oblig1
+
+
---
+++ src/test/main/java/oblig1/Oblig1UnitTest.java
@@ -0,0 +1,73 @@
+package oblig1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Denne klassen kan du bruke til hjelp under utvikling av din oblig.
+ * Lag små og enkle test-eksempler for å teste at metoden din fungerer som ønsket.
+ */
+class Oblig1UnitTest {
+
+    @org.junit.jupiter.api.Test
+    void maks() {
+        int[] a = {1, 4, 9, 7, 5, 34, 4, 8, 9, 23};
+
+        int maksimum = Oblig1.maks(a);
+        assertEquals(34, maksimum);
+    }
+
+    @org.junit.jupiter.api.Test
+    void ombyttinger() {
+        int[] a = {1, 2, 3, 5, 4};
+        int[] b = {9, 8, 7};
+        int[] c = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+        int ombyttinger = Oblig1.ombyttinger(c);
+        assertEquals(9, ombyttinger);
+    }
+
+    @org.junit.jupiter.api.Test
+    void antallUlikeSortert() {
+        assertEquals(true, false, "Implementer antallUlikeSortert og denne testen");
+    }
+
+    @org.junit.jupiter.api.Test
+    void antallUlikeUsortert() {
+        assertEquals(true, false, "Implementer antallUlikeUsortert og denne testen");
+    }
+
+    @org.junit.jupiter.api.Test
+    void delsortering() {
+        assertEquals(true, false, "Implementer delsortering og denne testen");
+    }
+
+    @org.junit.jupiter.api.Test
+    void rotasjon() {
+        assertEquals(true, false, "Implementer rotasjon og denne testen");
+    }
+
+    @org.junit.jupiter.api.Test
+    void flett() {
+        assertEquals(true, false, "Implementer flett og denne testen");
+    }
+
+    @org.junit.jupiter.api.Test
+    void indekssortering() {
+        assertEquals(true, false, "Implementer indekssortering og denne testen");
+    }
+
+    @org.junit.jupiter.api.Test
+    void tredjeMin() {
+        assertEquals(true, false, "Implementer tredjeMin og denne testen");
+    }
+
+    @org.junit.jupiter.api.Test
+    void bokstavNr() {
+        assertEquals(true, false, "Implementer bokstavNr og denne testen");
+    }
+
+    @org.junit.jupiter.api.Test
+    void inneholdt() {
+        assertEquals(true, false, "Implementer inneholdt og denne testen");
+    }
+}
